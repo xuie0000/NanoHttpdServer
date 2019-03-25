ca66da8fac038b9fdedc25348433e8c3d30b898f
Jie Xu <xujie@anzer.com.cn>
2019-03-25 14:09:36 +0800

    添加WebSocket

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-点击**开始**，然后用浏览器输入`http://<你ip>:8081`
+点击**开始**运行验证效果
 
-![效果截图](./screenshort.png)
+- Http
+用浏览器输入`http://<你ip>:8081`
+
+![http效果](./screenshort.png)
+
+- WebSocket
+输入`ws://<你ip>:8766`
+![wsd效果](./screenshort2.png)
--- app/src/main/java/com/xuie/nanohttpdserver/MainActivity.kt
+++ app/src/main/java/com/xuie/nanohttpdserver/MainActivity.kt
@@ -1,32 +1,60 @@
 package com.xuie.nanohttpdserver
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.format.Formatter
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private var isStarted: Boolean = false
-    private lateinit var androidWebServer: AndroidWebServer
+    private var isHttpStarted: Boolean = false
+    private lateinit var androidHttpServer: AndroidHttpServer
 
+    private var isWskStarted: Boolean = false
+    private lateinit var androidWebSocketServer: AndroidWebSocketServer
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        androidWebServer = AndroidWebServer()
+        androidHttpServer = AndroidHttpServer()
+        androidWebSocketServer = AndroidWebSocketServer(this)
 
-        val btnStart = findViewById<Button>(R.id.btn_start)
-        btnStart.setOnClickListener {
+        findViewById<TextView>(R.id.tv_http_request).text = "http://" + getIpAddress() + ":8081"
+        val btnHttpStart = findViewById<Button>(R.id.btn_http_start)
+        btnHttpStart.setOnClickListener {
             when {
-                isStarted -> {
+                isHttpStarted -> {
                     stopAndroidWebServer()
-                    btnStart.text = "开始"
+                    btnHttpStart.text = "开始"
                     Toast.makeText(this, "停止了", Toast.LENGTH_SHORT).show()
                 }
                 else -> {
-                    btnStart.text = "停止"
+                    btnHttpStart.text = "停止"
                     startAndroidWebServer()
+                    Toast.makeText(this, "开始了", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        findViewById<TextView>(R.id.tv_wsd_request).text = "ws://" + getIpAddress() + ":" + AndroidWebSocketServer.PORT
+        val btnWsdStart = findViewById<Button>(R.id.btn_wsd_start)
+        btnWsdStart.setOnClickListener {
+            when {
+                isWskStarted -> {
+                    androidWebSocketServer.closeAllConnections()
+                    btnWsdStart.text = "开始"
+                    Toast.makeText(this, "停止了", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    btnWsdStart.text = "停止"
+                    androidWebSocketServer.start()
                     Toast.makeText(this, "开始了", Toast.LENGTH_SHORT).show()
                 }
             }
@@ -35,19 +63,24 @@ class MainActivity : AppCompatActivity() {
 
     //region Start And Stop AndroidWebServer
     private fun startAndroidWebServer(): Boolean {
-        if (!isStarted) {
-            androidWebServer.start()
-            isStarted = true
+        if (!isHttpStarted) {
+            androidHttpServer.start()
+            isHttpStarted = true
             return true
         }
         return false
     }
 
     private fun stopAndroidWebServer(): Boolean {
-        if (isStarted) {
-            androidWebServer.stop()
+        if (isHttpStarted) {
+            androidHttpServer.stop()
         }
-        isStarted = false
+        isHttpStarted = false
         return true
+    }
+
+    private fun getIpAddress(): String {
+        val wm = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        return Formatter.formatIpAddress(wm.connectionInfo.ipAddress)
     }
 }
